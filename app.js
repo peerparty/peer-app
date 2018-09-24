de755a217ee3ff714f255f8d1fde2500779c9104
datafatmunger <info@peerparty.org>
2018-09-24 11:04:24 +0000

    clean-up

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ function doRequest(opts) {
   });
 };
 
-var members = [];
 var user = '_';
 
 /**
@@ -53,45 +52,18 @@ function isNumber(n) {
   return reg.test(n);
 }
 
-function memberName(memId) {
-  for(var i = 0; i < members.length; i++) {
-    if(members[i][0] == memId) return members[i][2];
-  }
-}
-
-function memberId(memName) {
-  for(var i = 0; i < members.length; i++) {
-    if(members[i][2] == memName) return members[i][0];
-  }
-}
-
 /**
  * APIish stuff - JBG
  */
-
-function appGetActivity(e) {
-  var t = e.target.className != 'item' ? e.target.parentNode : e.target;
-  var actId = t.getAttribute('data-id');
-  doRequest({ 'endpoint': '/activities?actId=' + actId })
-  .then((act) => {
-    appShowActivity(JSON.parse(act));
-  });
-}
 
 function appGetPost(e) {
   var t = e.target.className != 'item' ? e.target.parentNode : e.target
   var postId = t.getAttribute('data-id')
   doRequest({ 'endpoint': '/posts/' + postId })
     .then(post => appShowThread(JSON.parse(post)))
+    .catch(console.error)
 }
 
-
-function appGetActivities() {
-  doRequest({ 'endpoint': '/activities' })
-  .then((acts) => {
-    appShowActivities(JSON.parse(acts));
-  });
-}
 
 function appGetPosts() {
   doRequest({ 'endpoint': '/posts' })
@@ -100,47 +72,6 @@ function appGetPosts() {
   });
 }
 
-
-function appGetParticipants(actId) {
-  doRequest({ 'endpoint': '/participants?actId=' + actId })
-  .then((parts) => {
-    appShowParticipants(JSON.parse(parts));
-  });
-}
-
-function appGetVoteIds(actId) {
-  doRequest({ 'endpoint': '/votes?actId=' + actId })
-  .then((voteIdsStr) => {
-    var voteIds = JSON.parse(voteIdsStr);
-    if(voteIds.length == 0) {
-      appShowNoVotes();
-    } else {
-      for(var i = 0; i < voteIds.length; i++) {
-        appGetVote(voteIds[i]);
-      }
-    }
-  });
-}
-
-function appGetVote(voteId) {
-  doRequest({ 'endpoint': '/votes?voteId=' + voteId })
-  .then((vote) => {
-    appShowVote(JSON.parse(vote));
-  });
-}
-
-function appGetMembers() {
-  doRequest({ 'endpoint': '/members' })
-  .then((mems) => {
-    members = JSON.parse(mems);
-    for(var i = 0; i < members.length; i++) {
-      if(members[i][2] == user) {
-        document.querySelector('.header-bal .header-val').innerHTML = dec(members[i][4]);
-        document.querySelector('.header-prom .header-val').innerHTML = dec(members[i][5]);
-      }
-    }
-  });
-}
 
 async function appLogin(params) {
   doRequest({
@@ -154,66 +85,6 @@ async function appLogin(params) {
   .catch((err, res) => {
     console.log(err, res)
     document.querySelector('.error').innerHTML = "U FAILED.";
-  });
-}
-
-async function appAddActivity(params) {
-  doRequest({
-    'endpoint': '/activities',
-    'method': 'POST',
-    'params': params })
-  .then(() => {
-    appShowNotice('Thank you! Please allow some time for your activity to be mined. 🔨');
-  })
-  .catch((err, res) => {
-  });
-}
-
-async function appAddMember(params) {
-  doRequest({
-    'endpoint': '/members',
-    'method': 'POST',
-    'params': params })
-  .then(() => {
-    appShowNotice('Thank you! Please allow some time for your member to be mined. 🔨');
-  })
-  .catch((err, res) => {
-  });
-}
-
-async function appAddPart(params) {
-  doRequest({
-    'endpoint': '/participants',
-    'method': 'POST',
-    'params': params })
-  .then(() => {
-    appShowNotice('Thank you! Please allow some time for your participant to be mined. 🔨');
-  })
-  .catch((err, res) => {
-  });
-}
-
-async function appAddVote(params) {
-  doRequest({
-    'endpoint': '/votes',
-    'method': 'POST',
-    'params': params })
-  .then(() => {
-    appShowNotice('Thank you! Please allow some time for your vote to be mined. 🔨');
-  })
-  .catch((err, res) => {
-  });
-}
-
-async function appFinalize(params) {
-  doRequest({
-    'endpoint': '/finalize',
-    'method': 'POST',
-    'params': params })
-  .then(() => {
-    appShowNotice('Thank you! Please allow some time for activity to be mined. 🔨');
-  })
-  .catch((err, res) => {
   });
 }
 
@@ -257,8 +128,14 @@ function appShowComments(comment, elm) {
   elm.appendChild(comElm)
 }
 
+function appShowVotes(votes, elm) {
+  const up = votes.reduce((v, c) => { v.up ? c + 1 : c }, 0)
+  const down = votes.reduce((v, c) => { !v.up ? c + 1 : c }, 0)
+  elm.querySelector('.up').innerHTML = up
+  elm.querySelector('.down').innerHTML = down
+}
+
 function appShowThread(post) {
-  console.log(post)
   document.querySelector('.detail').innerHTML = ''
   appShowClose()
 
@@ -267,10 +144,21 @@ function appShowThread(post) {
   thread.querySelector('.title').innerHTML = post.title
   thread.querySelector('.description').innerHTML = post.description
 
-  var comments = thread.querySelector('.comments')
-  post.comments.forEach(comment => appShowComments(comment, comments))
+  var voteElm = document.querySelector('.templates .vote').cloneNode(true)
+  
+  if(post.votes) appShowVotes(post.votes, voteElm)
+  thread.appendChild(voteElm)
+
+  var comments = document.querySelector('.templates .comments').cloneNode(true)
+  thread.appendChild(comments)
+
+  if(post.comments) {
+    var comments = thread.querySelector('.comments')
+    post.comments.forEach(comment => appShowComments(comment, comments))
+  }
 
   document.querySelector('.detail').appendChild(thread)
+
 }
 
 function appShowPosts(posts) {
@@ -290,54 +178,6 @@ function appShowPosts(posts) {
   for(var i = 0; i < items.length; i++) {
     items[i].addEventListener('click', appGetPost, true)
   }
-}
-
-
-
-function appShowParticipants(parts) {
-  document.querySelector('.detail .participants').innerHTML = '';
-  // Participant list - JBG
-  for(var i = 0; i < parts.length; i++) {
-    var part = document.querySelector('.templates .participant').cloneNode(true);
-    part.innerHTML = memberName(parts[i]);
-    document.querySelector('.detail .participants').appendChild(part);
-
-        document.querySelector('.detail .add-part').addEventListener('click', () => {
-      appAddPart({
-        'actId': document.querySelector('.detail .activity').getAttribute('data-id'),
-        'memId': memberId(document.querySelector('.detail .sel-part').value),
-      });
-    }, true);
-  }
-
-  // Dropdown - JBG
-  for(var j = 0; j < members.length; j++) {
-    var part = false;
-    for(var i = 0; i < parts.length; i++) {
-      if(members[j][0] == parts[i]) part = true;
-    }
-    if(!part) {
-      var optPart = document.querySelector('.templates .opt-part').cloneNode(true)
-      optPart.innerHTML = members[j][2];
-      document.querySelector('.detail .sel-part').appendChild(optPart);
-    }
-  }
-}
-
-function appShowNoVotes() {
-  document.querySelector('.detail .votes').appendChild(
-    document.querySelector('.templates .no-votes').cloneNode(true)
-  );
-}
-
-function appShowVote(vote) {
-  if(vote[5]) return;
-  var voteElm = document.querySelector('.templates .vote').cloneNode(true);
-  voteElm.setAttribute('data-id', vote[0]);
-  voteElm.querySelector('.voter').innerHTML = memberName(vote[2]);
-  voteElm.querySelector('.promise').innerHTML = dec(vote[3]);
-  voteElm.querySelector('.just').innerHTML = vote[4];
-  document.querySelector('.detail .votes').appendChild(voteElm);
 }
 
 function appShowMenu() {
@@ -371,54 +211,8 @@ function appShowLogin() {
     }, false);
 }
 
-function appShowAddMember() {
-  document.querySelector('.detail').innerHTML = '';
-  document.querySelector('.detail').appendChild(
-    document.querySelector('.templates .add-member').cloneNode(true)
-  );
-}
-
-function appShowAddActivity() {
-  document.querySelector('.detail').innerHTML = '';
-  appShowClose();
-  document.querySelector('.detail').appendChild(
-    document.querySelector('.templates .add-activity').cloneNode(true)
-  );
-  document.querySelector('.detail .add-activity .button')
-    .addEventListener('click', () => {
-      var cost = document.querySelector('.detail input[name=cost]').value;
-      if(isNumber(cost)) { 
-	      appAddActivity({
-		"cost": parseFloat(cost) * 100,
-		"title": document.querySelector('.detail input[name=title]').value,
-		"description": document.querySelector('.detail input[name=description]').value,
-		"global": document.querySelector('.detail input[name=global]').checked
-	      });
-      } else {
-       alert('Please use integer number for cost. No "." or ",".');
-      }
-    }, false);
-}
-
-function appShowAddMember() {
-  document.querySelector('.detail').innerHTML = '';
-  appShowClose();
-  document.querySelector('.detail').appendChild(
-    document.querySelector('.templates .add-member').cloneNode(true)
-  );
-  document.querySelector('.detail .add-member .button')
-    .addEventListener('click', () => {
-      appAddMember({
-        "name": document.querySelector('.detail input[name=name]').value,
-        "addr": document.querySelector('.detail input[name=addr]').value
-      });
-    }, false);
-}
-
 function appShow() {
-  //appGetMembers();
   appShowHeader();
-  //appGetActivities();
   appGetPosts();
   appShowMenu();
 }
